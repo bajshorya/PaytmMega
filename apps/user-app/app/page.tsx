85f2cc9e931e57b4e1895e9bbf4fb1ef0dbca915
SHORYA BAJ <jain.shorya11@gmail.com>
2024-10-17 21:27:06 +0530

    prisma

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="font-extrabold text-9xl">hello</div>
+      <div className="text-9xl font-extrabold">hello</div>
     </div>
   );
 }
