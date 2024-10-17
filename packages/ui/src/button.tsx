74f874664e693151375835afb415be431bf807a2
SHORYA BAJ <jain.shorya11@gmail.com>
2024-10-17 20:31:40 +0530

    init

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -10,11 +10,14 @@ interface ButtonProps {
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
+    <div>
+      <div className="text-xl">Button</div>
+      <button
+        className={className}
+        onClick={() => alert(`Hello from your ${appName} app!`)}
+      >
+        {children}
+      </button>
+    </div>
   );
 };
