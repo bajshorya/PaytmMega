8fda5a1d8494c820f7dec7fb7198f1da7c5b2207
SHORYA BAJ <jain.shorya11@gmail.com>
2024-10-20 16:29:36 +0530

    setupped merchant side

---
+++ apps/merchant-side/app/layout.tsx
@@ -0,0 +1,18 @@
+import { Providers } from "@/providers";
+import { PrismaClient } from "@prisma/client";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <Providers>
+        <body className={inter.className}>{children}</body>
+      </Providers>
+    </html>
+  );
+}
