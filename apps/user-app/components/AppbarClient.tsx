dc1b114ac7165ab7099126071e21115caaed8b52
SHORYA BAJ <jain.shorya11@gmail.com>
2024-10-30 01:56:14 +0530

    appbar client

--- apps/user-app/components/AppbarClient.tsx
+++ apps/user-app/components/AppbarClient.tsx
@@ -8,15 +8,17 @@ export function AppbarClient() {
   const router = useRouter();
 
   return (
-    <div>
-      <Appbar
-        onSignin={signIn}
-        onSignout={async () => {
-          await signOut();
-          router.push("api/auth/signin");
-        }}
-        user={session.data?.user}
-      />
-    </div>
+    <>
+      <div>
+        <Appbar
+          onSignin={signIn}
+          onSignout={async () => {
+            await signOut();
+            router.push("api/auth/signin");
+          }}
+          user={session.data?.user}
+        />
+      </div>
+    </>
   );
 }
