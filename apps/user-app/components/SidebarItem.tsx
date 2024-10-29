cf691a33bf9d3cc3e861a5b823e6e11df75f0097
SHORYA BAJ <jain.shorya11@gmail.com>
2024-10-29 17:59:27 +0530

    checkpoint 18 with some bugs "Hydration failed"

---
+++ apps/user-app/components/SidebarItem.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+
+export const SidebarItem = ({
+  href,
+  title,
+  icon,
+}: {
+  href: string;
+  title: string;
+  icon: React.ReactNode;
+}) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  // Ensure pathname is defined before comparing
+  const selected = pathname ? pathname === href : false;
+
+  return (
+    <div
+      className={`flex items-center cursor-pointer p-2 pl-8 ${
+        selected ? "text-[#6a51a6] font-bold" : "text-slate-500"
+      }`}
+      onClick={() => router.push(href)}
+    >
+      <div className="pr-2">{icon}</div>
+      <div>{title}</div>
+    </div>
+  );
+};
